7ec665501b32253209e38a5d23169b2f2df2fc10
shotdsherrif <maurice.stephens@gmail.com>
2016-10-26 10:36:49 -0700

    further testing with $sce and transformResponse but error is not fixed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,69 +15,31 @@ angular.module('cornerCodeChallengeApp')
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
   })
 
-  .factory('instagram', ['$http',
-    function($http) {
-      return {
-        fetchFeed: function() {
+  .controller('MainCtrl',
+    ['$scope', '$http', '$sce',
+      function ($scope, $http, $sce) {
 
-          var endPoint = "https://www.instagram.com/JoinCornerstone/media";
+        $scope.data = {};
+        $scope.pics = [];
 
-          $http.jsonp(endPoint).success(function(response) {
-            callback(response.data);
-          })
-          .error(function(xhr, status, err) {
-            console.error(status, err);
-          });
+        var endPoint = 'https://www.instagram.com/joincornerstone/media'  ;
 
-          // $http({
-          //   method: 'GET',
-          //   url: 'https://www.instagram.com/JoinCornerstone/media',
-          //   headers: {
-          //     "Access-Control-Allow-Origin": "*",
-          //     "Access-Control-Allow-Headers": "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization",
-          //     "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-          //   }
-          // })
-          // .then(function success(response) {
-          //   console.log('response')
-          //   console.log(response)
-          //
-          // }, function error(error) {
-          //     console.log('error')
-          //     console.log(error)
-          //
-          // });
+        $sce.trustAsResourceUrl(endPoint);
 
+        $http.jsonp(endPoint, {param: {jsonpCallbackParam: 'angular.callbacks._0'}})
+        .then(function(response) {
+          console.log('response')
+          console.log(response)
 
-        }
+          $scope.data = response.data;
+
+          console.log('response.data')
+          console.log(response.data)
+        })
+        .catch(function(xhr, status, err) {
+          console.error(xhr,status, err);
+        });
+
       }
-    }
-  ])
-
-  .controller('MainCtrl', ['$scope', 'instagram',
-    function ($scope, instagram) {
-
-      $scope.data = {};
-      $scope.pics = [];
-
-      // Autocomplete test
-      // $scope.jobs = JobRetreiver.getjobs("...");
-      //   $scope.jobs.then(function(data){
-      //     $scope.jobs = data;
-      //   });
-      //
-      // $scope.getjobs = function(){
-      //   return $scope.jobs;
-      // }
-
-      // Instagram Feed
-      instagram.fetchFeed(function(data) {
-
-        console.log('data')
-        console.log(data)
-
-        $scope.pics = data;
-
-      });
-    }
-  ]);
+    ]
+  );
